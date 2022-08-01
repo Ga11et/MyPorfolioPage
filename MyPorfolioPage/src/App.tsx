d339abe63e87d6127daf7042eb494a5bf1a29f41
Fyodor <sivkov1998@yandex.ru>
2022-08-01 17:12:36 +0300

    added floroteka

--- MyPorfolioPage/src/App.tsx
+++ MyPorfolioPage/src/App.tsx
@@ -4,6 +4,7 @@ import lendingPhoto from './assets/lending.jpg'
 import SocialNetworkPhoto from './assets/socialNetwork.jpg'
 import weatherApp from './assets/weatherApp.jpg'
 import hotelBooking from './assets/hotelBooking.jpg'
+import floroteka from './assets/floroteka.jpg'
 import courses from './assets/courses.jpg'
 
 function App() {
@@ -20,12 +21,12 @@ const Header: FC<{}> = (props) => {
 }
 
 const projectsData = [
-  {id: 1, photo: hotelBooking, name: 'hotelBooking', href: 'https://ga11et.github.io/hotelBooking/'},
-  {id: 2, photo: courses, name: 'Courses', href: 'https://ga11et.github.io/Courses/'},
-  {id: 3, photo: weatherApp, name: 'Weather App', href: 'https://ga11et.github.io/WeatherApp/'},
-  {id: 4, photo: SocialNetworkPhoto, name: 'Social Network', href: 'https://ga11et.github.io/MyReactProject/'},
-  {id: 5, photo: lendingPhoto, name: 'Lending', href: 'https://ga11et.github.io/Lending/'},
-  {id: 6, photo: '', name: '...', href: ''},
+  {id: 1, photo: floroteka, name: 'Floroteka (Vue + express)', href: 'https://floroteka.netlify.app/'},
+  {id: 2, photo: hotelBooking, name: 'hotelBooking (React)', href: 'https://ga11et.github.io/hotelBooking/'},
+  {id: 3, photo: courses, name: 'Courses (React)', href: 'https://ga11et.github.io/Courses/'},
+  {id: 4, photo: weatherApp, name: 'Weather App', href: 'https://ga11et.github.io/WeatherApp/'},
+  {id: 5, photo: SocialNetworkPhoto, name: 'Social Network', href: 'https://ga11et.github.io/MyReactProject/'},
+  {id: 6, photo: lendingPhoto, name: 'Lending', href: 'https://ga11et.github.io/Lending/'}
 ]
 
 const Projects: FC<{}> = (props) => {
